5b8df78e8e6d66ef13947699112446ff569b9dee
MailMan <ainddal@gmail.com>
2024-05-05 12:27:22 +0300

    Initial commit

---
+++ src/test/java/ru/netology/auto1/PostRequestTest.java
@@ -0,0 +1,23 @@
+package ru.netology.auto1;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+
+public class PostRequestTest {
+    @Test
+    void testingPostMethod() {
+// Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("user")
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("admin"));
+    }
+}
