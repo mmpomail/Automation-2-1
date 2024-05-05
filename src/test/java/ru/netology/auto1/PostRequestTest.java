ef0da9435cc130b902d3b34546563c299b0be96b
MailMan <ainddal@gmail.com>
2024-05-05 12:36:01 +0300

    Good test

--- src/test/java/ru/netology/auto1/PostRequestTest.java
+++ src/test/java/ru/netology/auto1/PostRequestTest.java
@@ -18,6 +18,6 @@ public class PostRequestTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("admin"));
+                .body("data", equalTo("user"));
     }
 }
